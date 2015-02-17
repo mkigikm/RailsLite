eacdca370a6c497ba50a12e3c02281a36bbd58d6
Matthew Hicks <mkigikm@gmail.com>
2015-02-17 14:03:04 -0500

    adds rendering templates

--- lib/phase3/controller_base.rb
+++ lib/phase3/controller_base.rb
@@ -2,12 +2,17 @@ require_relative '../phase2/controller_base'
 require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
+require 'active_support/inflector'
 
 module Phase3
   class ControllerBase < Phase2::ControllerBase
     # use ERB and binding to evaluate templates
     # pass the rendered html to render_content
     def render(template_name)
+      file_name = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+      template = File.read(file_name)
+      rendered_template = ERB.new(template).result(binding)
+      render_content(rendered_template, 'text/html')
     end
   end
 end
