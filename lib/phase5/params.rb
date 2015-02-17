ac9db2fe7916eae65ca78d8b00903166a6156216
Matthew Hicks <mkigikm@gmail.com>
2015-02-17 16:25:05 -0500

    route adds path params to request params

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -11,7 +11,7 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
-      @params = route_params
+      @params = route_params.dup
       if req.query_string
         @params.merge!(parse_www_encoded_form(req.query_string))
       end
